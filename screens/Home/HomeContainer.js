7f4c53d02ea966280fc97091f4d97bd15fe87526
jinho1011 <gino9940@gmail.com>
2021-07-05 16:06:13 +0900

    feat: add blurred header

--- screens/Home/HomeContainer.js
+++ screens/Home/HomeContainer.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {BlurView} from '@react-native-community/blur';
+import styled from 'styled-components/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import HomePresenter from './HomePresenter';
 
+const Stack = createStackNavigator();
+
+const HeaderLeftContainer = styled.View``;
+
+const HeaderRightContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 80px;
+  margin-right: 20px;
+`;
+
 export default () => {
-  return <HomePresenter></HomePresenter>;
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomePresenter}
+        options={{
+          title: '',
+          headerLeft: () => <HeaderLeftContainer></HeaderLeftContainer>,
+          headerRight: () => (
+            <HeaderRightContainer>
+              <Ionicons
+                name={'chatbubble-ellipses'}
+                size={28}
+                color={'#B1B8C3'}
+              />
+              <Ionicons name={'notifications'} size={28} color={'#B1B8C3'} />
+            </HeaderRightContainer>
+          ),
+          headerTitleStyle: {
+            textAlign: 'center',
+          },
+          headerTransparent: true,
+          headerBackground: () => (
+            <BlurView
+              style={{
+                height: 56,
+              }}
+              blurType="light"
+              // blurAmount={100}
+              blurRadius={25}
+              reducedTransparencyFallbackColor="white"
+            />
+          ),
+        }}
+      />
+    </Stack.Navigator>
+  );
 };
