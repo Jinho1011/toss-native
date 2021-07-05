2eea2f2dcb4e9e1cc1e4b41412d06ecbc1012ed2
jinho1011 <gino9940@gmail.com>
2021-07-05 16:06:40 +0900

    feat: add User Section

--- screens/Home/HomePresenter.js
+++ screens/Home/HomePresenter.js
@@ -1,12 +1,113 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 import styled from 'styled-components/native';
+
+const HomeScroll = styled.ScrollView``;
+
+const HomeContainer = styled.View`
+  background-color: #ffffff;
+  margin-bottom: 200px;
+`;
+
+const HomeUser = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 80px 20px 20px 20px;
+`;
+
+const HomeUserLeft = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const HomeUserRight = styled.View``;
+
+const UserImage = styled.Image`
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+  margin-right: 10px;
+  background-color: grey;
+`;
+
+const UserNameContainer = styled.View`
+  margin-left: 10px;
+`;
+
+const UserName = styled.Text`
+  font-weight: bold;
+  font-size: 24px;
+`;
+
+const UserCredit = styled.Text`
+  font-size: 18px;
+  font-weight: 700;
+  color: #3182f6;
+`;
+
+const HomeRemit = styled.Pressable`
+  background-color: #3182f6;
+  justify-content: center;
+  align-items: center;
+  width: 78px;
+  height: 50px;
+  border-radius: 8px;
+`;
+
+const HomeRemitText = styled.Text`
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const HomeDivider = styled.View`
+  margin: 0 0 0 20px;
+  padding: 1px;
+  background-color: #e4e3e6;
+`;
+
+const HomeMargin = styled.View`
+  width: 100px;
+  height: 1000px;
+`;
 
 const Home = () => {
   return (
-    <View>
-      <Text>HOME</Text>
-    </View>
+    <HomeScroll>
+      <HomeContainer>
+        <HomeUser>
+          <HomeUserLeft>
+            <UserImage></UserImage>
+            <UserNameContainer>
+              <UserName>전진호</UserName>
+              <UserCredit>신용 점수 보기</UserCredit>
+            </UserNameContainer>
+          </HomeUserLeft>
+          <HomeUserRight>
+            <HomeRemit>
+              <HomeRemitText>송금</HomeRemitText>
+            </HomeRemit>
+          </HomeUserRight>
+        </HomeUser>
+        <HomeDivider></HomeDivider>
+        <HomeUser>
+          <HomeUserLeft>
+            <UserImage></UserImage>
+            <UserNameContainer>
+              <UserName>전진호</UserName>
+              <UserCredit>신용 점수 보기</UserCredit>
+            </UserNameContainer>
+          </HomeUserLeft>
+          <HomeUserRight>
+            <HomeRemit>
+              <HomeRemitText>송금</HomeRemitText>
+            </HomeRemit>
+          </HomeUserRight>
+        </HomeUser>
+        <HomeMargin></HomeMargin>
+      </HomeContainer>
+    </HomeScroll>
   );
 };
 
